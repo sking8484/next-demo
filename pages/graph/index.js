8e49eefb052086585e350b6eb00b5d0c5d0b05a9
sking8484 <sking8484@gmail.com>
2022-06-26 13:44:08 -0400

    None

--- pages/graph/index.js
+++ pages/graph/index.js
@@ -1,13 +1,18 @@
-
-
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+import Chart from '../../components/vl/Chart'
 export default function Graph(props) {
-  return <p>{JSON.stringify(props)}</p>
+  return (
+    <div className = "chart-container">
+      <Chart data = {props.data}/>
+    </div>
+  );
 }
-
 
 export async function getStaticProps(context) {
   let data = await fetch('https://cdn.jsdelivr.net/npm/vega-datasets@1.31.1/data/cars.json')
+  let jsonData = await data.json()
+  console.log(jsonData);
   return {
-    props: {data:data.json()}
+    props: {data:jsonData}
   }
 }
