8ab34800748beb4b5755bc3d3050dcdd52377b87
sking8484 <sking8484@gmail.com>
2022-06-26 11:44:54 -0400

    n

--- pages/graph/index.js
+++ pages/graph/index.js
@@ -1,5 +1,13 @@
 
 
-export default function Graph() {
-  return <p>Hello</p>
+export default function Graph(props) {
+  return <p>{JSON.stringify(props)}</p>
+}
+
+
+export async function getStaticProps(context) {
+  let data = await fetch('https://cdn.jsdelivr.net/npm/vega-datasets@1.31.1/data/cars.json')
+  return {
+    props: {data:data.json()}
+  }
 }
