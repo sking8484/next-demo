d555983977ace5c750db656c0619280e73a06c7b
sking8484 <sking8484@gmail.com>
2022-06-26 10:59:20 -0400

    Working with nav

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/global.css'
 
 export default function App({ Component, pageProps }) {
   return <div className = 'wrapper'>
+    <div className = 'navBar'>x</div>
     <Component className='component' {...pageProps} />
     </div>
 }
