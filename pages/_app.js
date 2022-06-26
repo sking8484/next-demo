41ec7913db6325fed202cc8a164e973d267571a9
sking8484 <sking8484@gmail.com>
2022-06-26 11:22:59 -0400

    navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import '../styles/global.css'
-
+import Link from 'next/link'
 export default function App({ Component, pageProps }) {
   return <div className = 'wrapper'>
-    <div className = 'navBar'></div>
+    <div className = 'navBar'>
+      <Link href = '/posts/'>
+        <a>Graph</a>
+      </Link>
+    </div>
     <Component className='component' {...pageProps} />
     </div>
 }
